c4e61b49bda002b52a7d6ccf44e2d249cb99ab51
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-11-16 16:31:50 +0300

    error: incompatible types: <anonymous OnClickListener> cannot be converted to Context

--- app/src/main/java/ru/avdeev/android/a5_1_1_saving_a_text_file/MainActivity.java
+++ app/src/main/java/ru/avdeev/android/a5_1_1_saving_a_text_file/MainActivity.java
@@ -2,11 +2,18 @@ package ru.avdeev.android.a5_1_1_saving_a_text_file;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
@@ -14,16 +21,21 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUESTE_CODE_PERMISSION_WRITE_STORAGE=11;
     private Random random = new Random();
     private ItemsDataAdapter adapter;
     private List<Drawable> images = new ArrayList<>();
     private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +50,21 @@ public class MainActivity extends AppCompatActivity {
 
         fillImages();
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                generateRandomItemData();
+                int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+                    generateRandomItemData();
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUESTE_CODE_PERMISSION_WRITE_STORAGE);
+
+                }
             }
         });
 
         adapter = new ItemsDataAdapter(this, null);
         listView.setAdapter(adapter);
-
-
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -57,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
     }
 
     private void fillImages() {
@@ -73,10 +90,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void generateRandomItemData() {
-        adapter.addItem(new ItemData(
-                images.get(random.nextInt(images.size())),
-                "Sample" + adapter.getCount(),
-                "Повторение - это по человечески, рекурсия божественна.", button));
+        if (isExternalStorageWritable()) {
+            File sampels = new File(getExternalFilesDir(null), "avdeev.txt");
+            FileWriter sampleWriter = null;
+            try {
+                sampleWriter = new FileWriter(sampels, true);
+                adapter.addItem(new ItemData(
+                        images.get(random.nextInt(images.size())),
+                        "Sample" + adapter.getCount(),
+                        "Повторение - это по человечески, рекурсия божественна.", button));
+                sampleWriter.append(adapter.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    sampleWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else {
+
+        }
     }
 
     private void showItemData(int position) {
@@ -85,5 +121,25 @@ public class MainActivity extends AppCompatActivity {
                 "Title: " + itemData.getTitle() + "\n" +
                         "Subtitle: " + itemData.getSubtitle(),
                 Toast.LENGTH_SHORT).show();
+    }
+
+    private void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUESTE_CODE_PERMISSION_WRITE_STORAGE:
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            } else {
+                Toast.makeText(this, getText(R.string.permission_not_obtained),Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state=Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)){
+            return true;
+        }return false;
     }
 }
